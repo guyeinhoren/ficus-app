184f657187b2f7ca0466cfc6b777962d3b156db8
guyeinhoren <guyeinhoren@gmail.com>
2025-10-04 08:13:42 +0300

    Initial Commit

---
+++ ficus/ficusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ficusApp.swift
+//  ficus
+//
+//  Created by גיא אינהורן on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ficusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ficusTests/ficusTests.swift
@@ -0,0 +1,17 @@
+//
+//  ficusTests.swift
+//  ficusTests
+//
+//  Created by גיא אינהורן on 04/10/2025.
+//
+
+import Testing
+@testable import ficus
+
+struct ficusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
